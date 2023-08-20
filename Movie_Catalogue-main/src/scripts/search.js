b81278da803933bcd5c33bab7918cfc8b7614a6f
Karoline <karoline.vabo@gmail.com>
2023-08-20 21:47:47 +0200

    cleanup

--- Movie_Catalogue-main/src/scripts/search.js
+++ Movie_Catalogue-main/src/scripts/search.js
@@ -1,14 +1,3 @@
 import './search.css';
 import './search.html';
 import React, { Component } from 'react';
-
-/*
-function isClicked() {
-    this.state.isClicked = true;
-    let changeStyles;
-    changeStyles = () => {
-        let element = document.getElementById('button')
-        ReactDOM.findDOMNode = function () {};
-        ReactDOM.findDOMNode(element).style.color = this.state.isClicked ? '#FCD53F' : '#e6b807'
-    }
-}*/
